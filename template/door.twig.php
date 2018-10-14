4cee019e5cec9970944e839b9865c5c26a3e5285
Makkssimka <maxvolgstom@yandex.ru>
2018-10-14 10:36:18 +0300

    Слайдер систем дверей

--- template/door.twig.php
+++ template/door.twig.php
@@ -64,6 +64,33 @@
     </div>
 </div>
 
+<div class="catalog-system-wrapper">
+    <div class="container">
+        <h4>системы</h4>
+        <p>Ваша дверь может быть функциональной и уникальной. Доступные системы в коллекции {{ door.name }}:</p>
+        <div uk-slider class="catalog-slider-system">
+            <ul class="uk-slider-items">
+                <li><img src="/public/images/system-1.svg" alt="Распашная дверь, открывание на себя"><p>Распашная дверь, открывание на себя</p></li>
+                <li><img src="/public/images/system-2.svg" alt="Раздвижная дверь вдоль стены в пенал"><p>Раздвижная дверь вдоль стены в пенал</p></li>
+                <li><img src="/public/images/system-3.svg" alt="Дверь-книжка"><p>Дверь-книжка</p></li>
+                <li><img src="/public/images/system-4.svg" alt="Обрамление проема"><p>Обрамление проема</p></li>
+                <li><img src="/public/images/system-5.svg" alt="Раздвижная перегородка вдоль стены"><p>Раздвижная перегородка вдоль стены</p></li>
+                <li><img src="/public/images/system-6.svg" alt="Фрамуга верхняя"><p>Фрамуга верхняя</p></li>
+                <li><img src="/public/images/system-7.svg" alt="Фрамуга боковая"><p>Фрамуга боковая</p></li>
+                <li><img src="/public/images/system-8.svg" alt="Раздвижная дверь вдоль стены"><p>Раздвижная дверь вдоль стены</p></li>
+            </ul>
+            <div class="catalog-slider-nav">
+                <div class="arrows">
+                    <a href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                    <a href="#" uk-slidenav-next uk-slider-item="next"></a>
+                </div>
+                <a class="orange-but" href="#">подробнее о системах</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
 <!-- Блок подвала -->
 {% include("bloks/footer.twig.php") %}
 
